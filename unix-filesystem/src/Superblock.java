652dfd42cf02a200922943041cb4d08100f32f87
John Steele <programjsteele@gmail.com>
2011-12-01 19:23:29 -0800

    The files needed to be implemented.

---
+++ unix-filesystem/src/Superblock.java
@@ -0,0 +1,18 @@
+
+public class Superblock {
+
+	/**
+	 * The number of disk blocks.
+	 */
+	public int totalBlocks; 
+	
+	/**
+	 * The number of inodes.
+	 */
+	public int totalInodes;
+	
+	/**
+	 * The block number of the free list's head.
+	 */
+	public int freeList;
+}
